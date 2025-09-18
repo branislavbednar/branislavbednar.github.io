a5e31a4dd8f0f3c4ac2dbe030219399be3426bf9
Branislav Bednár <branislav.bednar@visma.com>
2025-09-18 20:51:58 +0200

    finalization of experience section v2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {LandingPage} from './landing-page/landing-page';
+import {AboutMe} from './about-me/about-me';
+import {Experience} from './experience/experience';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', component: LandingPage},
+  {path: 'about-me', component: AboutMe},
+  {path: 'experience', component: Experience},
+  {path: '**', redirectTo: ''}
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,19 +1,42 @@
-import { Component, signal } from '@angular/core';
-import {Experience} from './experience/experience';
-import {AboutMe} from './about-me/about-me';
-import {LandingPage} from './landing-page/landing-page';
+import {Component, signal} from '@angular/core';
+import {RouterOutlet, Router, RouterLink, RouterLinkActive} from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    Experience,
-    AboutMe,
-    LandingPage
+    RouterOutlet,
+    RouterLink,
+    RouterLinkActive
   ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
+export class App{
   protected readonly title = signal('branislavbednar.github.io');
+
+  constructor(private readonly router: Router) {
+  }
+
+  scrollToSection(sectionId: string) {
+    const element = document.getElementById(sectionId);
+    if (element) {
+      element.scrollIntoView({behavior: 'smooth'});
+    }
+  }
+
+  showCollapsedMenu() {
+    const menu = document.getElementById('collapsedMenu');
+    if (menu) {
+      menu.classList.toggle('w3-hide');
+    }
+  }
+
+  collapseMenu() {
+    const menu = document.getElementById('collapsedMenu');
+    if (menu) {
+      menu.classList.add('w3-hide');
+    }
+  }
 }
+
